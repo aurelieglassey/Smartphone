e0962004139ad6bed9553557607c2a5b022e803d
aurelieglassey <aurelie.glassey@students.hevs.ch>
2017-06-13 10:55:11 +0200

    Javadoc
    commenter les classes, méthodes, ...

--- src/smartphone/SmartScrollPane.java
+++ src/smartphone/SmartScrollPane.java
@@ -4,21 +4,36 @@ import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.JScrollPane;
-
+/**
+ * Classe qui met en forme un ScrollPane pour la liste de contact et la liste de musique
+ * @author Fabien
+ *
+ */
 public class SmartScrollPane extends JScrollPane
 {
+	/**
+	 * Constructeur de la classe SmartScrollPane sans paramètres
+	 */
 	public SmartScrollPane()
 	{
 		super();
 		customizeScrollPane();
 	}
 
+	/**
+	 * Constructeur de la classe SmartScrollPane avec un paramètre view
+	 * @param view
+	 */
 	public SmartScrollPane( Component view )
 	{
 		super( view );
 		customizeScrollPane();
 	}
 	
+	/**
+	 * Méthode qui met en forme le scrollPane.
+	 * Adapte la largeur, enlève les bords et met en forme la couleur de fond d'un ScrollPane
+	 */
 	public void customizeScrollPane()
 	{
 		getVerticalScrollBar().setPreferredSize(
--- src/smartphone_test/SmartphoneTest.java
+++ src/smartphone_test/SmartphoneTest.java
@@ -8,11 +8,17 @@ import org.junit.Test;
 
 import smartphone.Contact;
 
+/**
+ * SmartPhoneTest est une classe créée pour tester les JUnit
+ * @author Aurélie Glassey
+ *
+ */
 public class SmartphoneTest
 {
 
 	/**
-	 * Méthodes de test avec JUnit pour tester les paramètres d'entrées d'un Contact
+	 * Méthode pour tester les paramètres d'entrée s'il sont null ou non lors de la création d'un Contact
+	 * Test : si une exception est quand même appelée alors que tous les paramètres obligatoires sont rentrés
 	 */
 	@Test
 	public void nullConstructor()
@@ -28,6 +34,11 @@ public class SmartphoneTest
 		}
 	}
 	
+	/**
+	 * Méthode pour tester le paramètre du numéro de téléphone d'un contact
+	 * Le numéro de téléphone n'est pas obligatoire. 
+	 * Test : si une exception est appelée lorsqu'un numéro de téléphone n'est pas rempli
+	 */
 	@Test
 	public void noPhoneNumber()
 	{		
